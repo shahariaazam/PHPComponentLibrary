f3d30922fc4f16887009e1189b951e2bd76c074e
Shaharia Azam <shaharia.azam@gmail.com>
2013-02-20 13:15:15 +0600

    Basic structure build for password encryption.

--- src/Security/PasswordEncrypt.php
+++ src/Security/PasswordEncrypt.php
@@ -3,5 +3,36 @@ namespace Security\PasswordEncrypt;
 
 class PasswordEncrypt
 {
+    public $_salt;
+    protected $_hash;
+
+    function __construct()
+    {
+        return null;
+    }
+
+    function Encrypt($password = null)
+    {
+        $salt = $this->generateSalt(null);
+        $encrypted = crypt($password, $salt);
+        return array('Salt' => $salt, 'Encrypted' => $encrypted);
+    }
+
+    /**
+     * @param null $method
+     * @return int
+     */
+    protected function generateSalt()
+    {
+        /**
+         * @todo generate salt in various methods by researching internet
+         */
+        return $this->_salt;
+    }
 
 }
+
+$obj = new \Security\PasswordEncrypt\PasswordEncrypt;
+$obj->_salt = "ThisIsTheKey";
+$EncryptedPassword = $obj->Encrypt('verysecretpassword');
+var_dump($EncryptedPassword);
